7645decb6dc36ebdc95142ffc194c58dda1a26b0
Hossein <hosseinteymouri069@gmail.com>
2019-07-08 10:36:08 +0430

    ignore main.c

--- Src/main.c
+++ Src/main.c
@@ -56,7 +56,7 @@ HAL_StatusTypeDef state;
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+//This is a simple comment
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
